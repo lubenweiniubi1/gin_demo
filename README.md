a827af402cd5a11cc2888ea76b209779a7c36e3f
卢本伟牛批 <panlinfeng@hive-intel.com>
2020-09-05 00:15:31 +0800

    1

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 常用库
 
 https://www.liwenzhou.com/posts/Go/go_menu/#autoid-0-3-0
+
+作业：
+
+
+
+1. restful 风格api看了
+2. http协议简单看一下
+3. 阮一峰的restful api 看一下都在这个里面
+
+> https://www.liwenzhou.com/posts/Go/Gin_framework/
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,9 +10,25 @@ func main() {
 	r := gin.Default() //返回默认路由引擎
 
 	//指定用户使用GET请求访问/ping 时，执行func。。这个函数
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{ //H是格 map，自己点进去看
-			"message": "pong",
+			"method": "get",
+		})
+	})
+	r.POST("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "POST",
+		})
+	})
+
+	r.PUT("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "PUT",
+		})
+	})
+	r.DELETE("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "DELETE",
 		})
 	})
 	//启动服务
