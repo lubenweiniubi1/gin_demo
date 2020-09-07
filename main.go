005d843eb02c97066bd188f9c193b53ae502f9ef
卢本伟牛批 <panlinfeng@hive-intel.com>
2020-09-07 08:25:06 +0800

    文件上传

--- main.go
+++ main.go
@@ -3,52 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path"
 
 	"github.com/gin-gonic/gin"
 )
 
-type UserInfo struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-} //应用反射，让别人拿到你的字段 ，首字母大写 ，并指定tag ,requred表示必须的，不传会报错 ; json字段表示 如果穿的是json文件，就根据这个来选择 username属性，
-
 //go mod tidy 看依赖包是否在go mod 中列出来
 func main() {
 	r := gin.Default()
-
-	r.GET("/user", func(c *gin.Context) {
-		// 这两个都不能忽略，不然就访问不到该路由
-
-		var user UserInfo //生命一个user【info类型变量
-
-		err := c.ShouldBind(&user) // 函数都是值拷贝，这里传的是个拷贝，应该传一个地址
+	r.LoadHTMLFiles("./index.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.POST("/upload", func(c *gin.Context) {
+		//从请求中读取文件
+		fileReader, err := c.FormFile("f1") //从请求中获取携带的参数
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"err": err.Error(),
 			})
 		} else {
+			//将文件保存到本地
+			// dst := fmt.Sprintf("./%s",f.Filename)
+			dst := path.Join("./", fileReader.Filename)
+			fmt.Printf(fileReader.Filename)         //自拍.jpg
+			_ = c.SaveUploadedFile(fileReader, dst) //短线就不加冒号了 ，这里不处理错误
 			c.JSON(http.StatusOK, gin.H{
-				"status": "ok",
+				"status": "OK",
 			})
 		}
-		fmt.Println(user)
+		//将读取出的文件保存到本地
 	})
-	r.POST("/user", func(c *gin.Context) {
-		var user UserInfo
-
-		err := c.ShouldBind(&user)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"err": err.Error(),
-			})
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"status": "ok",
-			})
-		}
-		fmt.Println(user)
-
-	})
-
-	r.Run()
+	r.Run(":8080")
 }
