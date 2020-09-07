3f0e12b1b408a28f97dc8f954c907c0a0b32d2bd
卢本伟牛批 <panlinfeng@hive-intel.com>
2020-09-07 23:00:47 +0800

    出识中间件

--- main.go
+++ main.go
@@ -1,40 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+//handlefunc 类型
+func indexHandler(c *gin.Context) {
+	fmt.Println("index")
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "index",
+	})
+}
+
+//定义一个中间件 m1
+func m1(c *gin.Context) {
+	fmt.Println("m1 in ...")
+}
+
 //go mod tidy 看依赖包是否在go mod 中列出来
 func main() {
 	r := gin.Default()
 
-	r.GET("/index", func(c *gin.Context) {
-		// c.JSON(http.StatusOK, gin.H{
-		// 	"status": "ok",
-		// })
-		c.Redirect(http.StatusMovedPermanently, "http://www.sogo.com") //这里地址都变了
-	})
+	// func (group *RouterGroup) GET(relativePath string, handlers ...HandlerFunc) IRoutes {
+	r.GET("/index", m1, indexHandler)
 
-	r.GET("/a", func(c *gin.Context) {
-		c.JSON(http.StatusPermanentRedirect, gin.H{
-			"message": "你好 ，这里到路由a了",
-		})
-		c.Request.URL.Path = "/b" //把请求的URI修改
-		r.HandleContext(c)        //继续后续处理
-	})
-	r.GET("/b", func(c *gin.Context) {
-		c.JSON(http.StatusPermanentRedirect, gin.H{
-			"message": "你好 ，这里重定向到路由b了",
-		})
-		c.Request.URL.Path = "/c"
-		r.HandleContext(c)
-	})
-	r.GET("/c", func(c *gin.Context) {
-		c.JSON(http.StatusPermanentRedirect, gin.H{
-			"message": "这里是c",
-		})
-	})
 	r.Run(":8080")
 }
