2502587169629198d9b5418e2287d99d316dfd47
卢本伟牛批 <panlinfeng@hive-intel.com>
2020-09-07 21:54:15 +0800

    路由和路由组

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"path"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,28 +9,32 @@ import (
 //go mod tidy 看依赖包是否在go mod 中列出来
 func main() {
 	r := gin.Default()
-	r.LoadHTMLFiles("./index.html")
+
 	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
+		// c.JSON(http.StatusOK, gin.H{
+		// 	"status": "ok",
+		// })
+		c.Redirect(http.StatusMovedPermanently, "http://www.sogo.com") //这里地址都变了
 	})
-	r.POST("/upload", func(c *gin.Context) {
-		//从请求中读取文件
-		fileReader, err := c.FormFile("f1") //从请求中获取携带的参数
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"err": err.Error(),
-			})
-		} else {
-			//将文件保存到本地
-			// dst := fmt.Sprintf("./%s",f.Filename)
-			dst := path.Join("./", fileReader.Filename)
-			fmt.Printf(fileReader.Filename)         //自拍.jpg
-			_ = c.SaveUploadedFile(fileReader, dst) //短线就不加冒号了 ，这里不处理错误
-			c.JSON(http.StatusOK, gin.H{
-				"status": "OK",
-			})
-		}
-		//将读取出的文件保存到本地
+
+	r.GET("/a", func(c *gin.Context) {
+		c.JSON(http.StatusPermanentRedirect, gin.H{
+			"message": "你好 ，这里到路由a了",
+		})
+		c.Request.URL.Path = "/b" //把请求的URI修改
+		r.HandleContext(c)        //继续后续处理
+	})
+	r.GET("/b", func(c *gin.Context) {
+		c.JSON(http.StatusPermanentRedirect, gin.H{
+			"message": "你好 ，这里重定向到路由b了",
+		})
+		c.Request.URL.Path = "/c"
+		r.HandleContext(c)
+	})
+	r.GET("/c", func(c *gin.Context) {
+		c.JSON(http.StatusPermanentRedirect, gin.H{
+			"message": "这里是c",
+		})
 	})
 	r.Run(":8080")
 }
