c27c74728a4b3594a093d9dba887b1dc8c5dabc4
卢本伟牛批 <panlinfeng@hive-intel.com>
2020-09-17 22:59:28 +0800

    zz

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module gin_demo
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.16
 	github.com/jinzhu/now v1.1.1 // indirect
-	github.com/mattn/go-sqlite3 v1.14.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,41 +1,89 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+) //这里涉及到gorm 版本变更问题，目前使用视频里的东西，
+
+var (
+	DB *gorm.DB
 )
 
-//首先定义模型：
-type User struct {
-	gorm.Model
-	Name   string
-	Age    int64
-	Active bool
+//Todo Model
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
+func initMySQL() (err error) {
+	dsn := "root:root1234@(127.0.0.1:3306)/db1?charset=utf8mb4&loc=Local&parseTime=true"
+	DB, err = gorm.Open("mysql", dsn) //不要冒号等于重复生命，已经声明全局了
+	if err != nil {
+		return
+	}
+
+	return DB.DB().Ping() //ping的通就返回nil
 }
 
 func main() {
+	//创建数据库
+	//创建bubble
 	//连接数据库
-	db, err := gorm.Open("mysql", "root:root1234@(127.0.0.1:3306)/db1?charset=utf8mb4")
+	err := initMySQL()
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer DB.Close()
+	//模型绑定
+	DB.AutoMigrate(&Todo{})
+	r := gin.Default()
+	//v1
+	v1Group := r.Group("v1")
+	{
+		//代办事项
+		//添加
+		v1Group.POST("/todo", func(c *gin.Context) {
+			//前端页面填写代办事项，会发请求到这里
+			//1.把请求搞出来
+			var todo Todo
+			c.BindJSON(&todo)
+			//2.存入数据库
+			if err = DB.Create(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{
+					"error": err.Error(),
+				})
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"code": 2000,
+					"msg":  "success",
+					"data": todo,
+				})
+			}
+			//3. 返回响应
 
-	db.AutoMigrate(&User{})
+		})
+		//查看所有代办事项
+		v1Group.GET("/todo", func(c *gin.Context) {
 
-	// user1 := User{Name: "qimi", Age: 20, Active: true}
-	// user2 := User{Name: "uzi", Age: 18, Active: false}
+		})
+		//查看某一个待办事项
+		v1Group.GET("/todo/:id", func(c *gin.Context) {
 
-	// db.Create(&user2)
-	// db.Create(&user1)
+		})
+		//修改
+		v1Group.PUT("/todo/:id", func(c *gin.Context) {
 
-	var user User
-	db.First(&user)
+		})
+		//删除
+		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
 
-	//6.更新
-	// user.Name = "1"
-	// db.Save(&user) //默认修改所有字段,其他没设的字段就没了
+		})
+	}
 
-	// db.Debug().Delete(&user)
-	// db.Unscoped().Delete(&user)
+	r.Run()
+
 }
